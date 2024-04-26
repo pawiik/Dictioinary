24250a3d1ab91b0c92b4777eb720fabf8d03b631
pawiik <paweld4642@gmail.com>
2024-04-26 17:29:29 +0200

    added trie node implementation

--- KahootChallenge/src/main/java/com/dabrowski/pawel/TrieNode.java
+++ KahootChallenge/src/main/java/com/dabrowski/pawel/TrieNode.java
@@ -1,4 +1,14 @@
 package com.dabrowski.pawel;
 
 public class TrieNode {
+    //  Our tree will contain only English small letters
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = null;
+        }
+    }
 }
