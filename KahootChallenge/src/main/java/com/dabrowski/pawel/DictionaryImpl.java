9f17b38d8769a7bf73af445f20281af25fa56c33
pawiik <paweld4642@gmail.com>
2024-04-27 17:15:28 +0200

    finished finding single match

--- KahootChallenge/src/main/java/com/dabrowski/pawel/DictionaryImpl.java
+++ KahootChallenge/src/main/java/com/dabrowski/pawel/DictionaryImpl.java
@@ -1,15 +1,18 @@
 package com.dabrowski.pawel;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class DictionaryImpl implements Dictionary {
+    private final TrieNode root;
 
     public DictionaryImpl() {
-
+        root = new TrieNode();
     }
 
     public DictionaryImpl(List<String> words) {
+        root = new TrieNode();
         for(String word : words) {
             this.addElement(word);
         }
@@ -17,7 +20,15 @@ public class DictionaryImpl implements Dictionary {
 
     @Override
     public void addElement(String newElement) {
-
+        TrieNode current = root;
+        for (int i = 0; i < newElement.length(); i++) {
+            int index = newElement.charAt(i) - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
+        }
+        current.isEndOfWord = true;
     }
 
     @Override
@@ -32,7 +43,23 @@ public class DictionaryImpl implements Dictionary {
 
     @Override
     public List<String> getElement(String letters) {
+        List<String> result = new ArrayList<>();
+        TrieNode current = root;
+        StringBuilder found = new StringBuilder();
 
-        return Collections.emptyList();
+        // We are checking all letters of the given word
+        for (char c : letters.toCharArray()) {
+            int index = c - 'a';
+            if (current.children[index] == null) {
+                return result; // No match found
+            }
+            found.append(c);
+            current = current.children[index];
+        }
+
+        result.add(found.toString());
+
+        return result;
     }
+
 }
